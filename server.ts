7056710e954e70190dd31705b24618c243075c00
CutzuDev <alexfarkas05@gmail.com>
2024-01-28 22:03:50 +0200

    updt

--- server.ts
+++ server.ts
@@ -2,10 +2,8 @@ import requestFunction from "./request";
 
 const server = Bun.serve({
   async fetch(request, server) {
-    if (await request.json()) {
-      const reqJ = await request.json();
-      console.log(reqJ);
-    }
+    const reqJ = await request.json();
+    console.log(reqJ);
     return new Response("hi");
   },
 });
